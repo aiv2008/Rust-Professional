d6917e60e02b5cd3fdbaa075056d3268c162ba9b
aiv2008 <aiv2005@sina.com>
2025-03-13 14:22:04 +0800

    easy algorithm17 finsished

--- exercises/easy/algorithm17.rs
+++ exercises/easy/algorithm17.rs
@@ -11,10 +11,77 @@
 
 use std::fmt::{self, Display, Formatter};
 
+fn distinct(merge_v: Vec<i32>)-> Vec<i32>{
+    let mut result = Vec::<i32>::new();
+    let mut i = 0;
+    let mut j=1;
+    let mut l = i;
+    if merge_v.len()>1{
+         while l< merge_v.len(){
+            println!("i={}", i);
+            j = i + 1;
+            while j <  merge_v.len(){
+                if merge_v[i] == merge_v[j]{
+                    if!result.contains(&merge_v[i]){
+                        result.push(merge_v[i]);
+                    }
+                    i = j + 1;
+                    break;
+                }else{
+                    // result.push(merge_v[i]);
+                    j += 1;
+                }
+            }
+            if i <= l + 1{
+                i += 1;
+                l += 1;
+            }else{
+                l = i;
+            }
+        }
+    }
+    result
+}
+
 pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
     // TODO: Implement the logic to find the intersection of two arrays
-    Vec::new() // Placeholder return value
+    let mut v1 = Vec::<i32>::new();
+    let mut v2 = Vec::<i32>::new();
+    for a in nums1 {
+        v1.push(a);
+    }
+    for a in nums2 {
+        v2.push(a);
+    }
+    v1.sort();
+    v2.sort();
+    let mut dis_v1 = Vec::<i32>::new();
+    for a in v1{
+        if !dis_v1.contains(&a){
+            dis_v1.push(a);
+        }
+    }
+    let mut dis_v2 = Vec::<i32>::new();
+    for a in v2{
+        if !dis_v2.contains(&a){
+            dis_v2.push(a);
+        }
+    }
+    
+    let mut merge_v = Vec::<i32>::new();
+    for a in dis_v1 {
+        merge_v.push(a);
+    }
+    for a in dis_v2 {
+        merge_v.push(a);
+    } 
+    merge_v.sort();
+    println!("merge={:?}",merge_v );
+    let result = distinct(merge_v);
+    result
+    // Vec::new() // Placeholder return value
 }
+
 
 #[cfg(test)]
 mod tests {
