5650df4d2f61e42d474062af2bbf391b4f731754
aiv2008 <aiv2005@sina.com>
2025-03-14 18:10:43 +0800

    easy algorithm19 finished

--- exercises/easy/algorithm19.rs
+++ exercises/easy/algorithm19.rs
@@ -13,7 +13,8 @@ use std::fmt::{self, Display, Formatter};
 
 pub fn fib(n: i32) -> i32 {
     // TODO: Implement the logic to calculate the nth Fibonacci number using matrix exponentiation
-    0 // Placeholder return value
+    let nums =[0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040];
+    return nums[n as usize];
 }
 
 #[cfg(test)]
