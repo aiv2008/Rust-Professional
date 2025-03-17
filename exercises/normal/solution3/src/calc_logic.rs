255ec625c4cfc18e02a97061fd913e3df371f97d
aiv2008 <aiv2005@sina.com>
2025-03-17 15:33:21 +0800

    normal solution3 finished

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,11 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
     // TODO: 这里写逻辑
-    todo!()
+    let mut j = 0;
+    let mut sum = 1.0;
+    for i in (365-n)..365{
+        sum *= ((365-j) as f64)/(365 as f64);
+        j += 1;
+    }
+    1.0 - sum
+    // todo!()
 }
