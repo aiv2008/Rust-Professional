5381e24f8ff0e61edcc19d8640e32e38ff5031d5
aiv2008 <aiv2005@sina.com>
2025-03-17 16:48:02 +0800

    normal solution4 finished

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,22 @@
 pub fn dp_rec_mc(amount: u32) -> u32 {
     // TODO: 这里写逻辑
-    todo!()
+    const CASHES: [u32; 8] = [1, 2, 5, 10, 20, 30, 50, 100];
+    let mut v = Vec::<u32>::new();
+    let mut j: i32 = (CASHES.len() - 1) as i32;
+    let mut quantity:u32 = 0;
+    let mut remain = amount;
+    while remain >= 0 && j >= 0{
+        if CASHES[j as usize] <= remain{
+            println!("sdfsd");
+            v.push(CASHES[j as usize] );
+            remain -= CASHES[j as usize];
+            quantity += 1;
+        }else{
+            j -= 1;
+        }
+        
+    }
+    println!("v={:?}", v);
+    quantity
+    // todo!()
 }
