8a83302d7363762756590b09ab7a71ebdc52bd41
aiv2008 <aiv2005@sina.com>
2025-03-19 18:05:21 +0800

    hard solutiont2 writing

--- exercises/hard/solutiont2/src/main.rs
+++ exercises/hard/solutiont2/src/main.rs
@@ -2,7 +2,176 @@
 
 use std::cmp::Reverse;
 
+/*
+	double linked list reverse
+	This problem requires you to reverse a doubly linked list
+*/
 
+
+use std::fmt::{self, Display, Formatter};
+use std::ptr::NonNull;
+use std::vec::*;
+
+#[derive(Debug)]
+struct Node<T> {
+    val: T,
+    next: Option<NonNull<Node<T>>>,
+    prev: Option<NonNull<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(t: T) -> Node<T> {
+        Node {
+            val: t,
+            prev: None,
+            next: None,
+        }
+    }
+}
+#[derive(Debug)]
+struct LinkedList<T> {
+    length: u32,
+    start: Option<NonNull<Node<T>>>,
+    end: Option<NonNull<Node<T>>>,
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            length: 0,
+            start: None,
+            end: None,
+        }
+    }
+
+    pub fn add(&mut self, obj: T) {
+        let mut node = Box::new(Node::new(obj));
+        node.next = None;
+        node.prev = self.end;
+        let node_ptr = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        match self.end {
+            None => self.start = node_ptr,
+            Some(end_ptr) => unsafe { (*end_ptr.as_ptr()).next = node_ptr },
+        }
+        self.end = node_ptr;
+        self.length += 1;
+    }
+
+    pub fn get(&mut self, index: i32) -> Option<&T> {
+        self.get_ith_node(self.start, index)
+    }
+
+    fn get_ith_node(&mut self, node: Option<NonNull<Node<T>>>, index: i32) -> Option<&T> {
+        match node {
+            None => None,
+            Some(next_ptr) => match index {
+                0 => Some(unsafe { &(*next_ptr.as_ptr()).val }),
+                _ => self.get_ith_node(unsafe { (*next_ptr.as_ptr()).next }, index - 1),
+            },
+        }
+    }
+	pub fn reverse(&mut self){
+		// TODO
+        let mut move_node = self.start;
+        self.start = self.end;
+        self.end = move_node;
+        while let Some(_m_node) =  move_node{
+            unsafe {
+                let temp_node = ( *_m_node.as_ptr()).next ;
+               ( *_m_node.as_ptr()).next = (*_m_node.as_ptr()).prev;  
+               (*_m_node.as_ptr()).prev = temp_node; 
+               move_node = (*_m_node.as_ptr()).prev ;
+            }
+        }
+	}
+}
+
+impl<T> Display for LinkedList<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.start {
+            Some(node) => write!(f, "{}", unsafe { node.as_ref() }),
+            None => Ok(()),
+        }
+    }
+}
+
+impl<T> Display for Node<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.next {
+            Some(node) => write!(f, "{}, {}", self.val, unsafe { node.as_ref() }),
+            None => write!(f, "{}", self.val),
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::LinkedList;
+
+//     #[test]
+//     fn create_numeric_list() {
+//         let mut list = LinkedList::<i32>::new();
+//         list.add(1);
+//         list.add(2);
+//         list.add(3);
+//         println!("Linked List is {}", list);
+//         assert_eq!(3, list.length);
+//     }
+
+//     #[test]
+//     fn create_string_list() {
+//         let mut list_str = LinkedList::<String>::new();
+//         list_str.add("A".to_string());
+//         list_str.add("B".to_string());
+//         list_str.add("C".to_string());
+//         println!("Linked List is {}", list_str);
+//         assert_eq!(3, list_str.length);
+//     }
+
+//     #[test]
+//     fn test_reverse_linked_list_1() {
+// 		let mut list = LinkedList::<i32>::new();
+// 		let original_vec = vec![2,3,5,11,9,7];
+// 		let reverse_vec = vec![7,9,11,5,3,2];
+// 		for i in 0..original_vec.len(){
+// 			list.add(original_vec[i]);
+// 		}
+// 		println!("Linked List is {}", list);
+// 		list.reverse();
+// 		println!("Reversed Linked List is {}", list);
+// 		for i in 0..original_vec.len(){
+// 			assert_eq!(reverse_vec[i],*list.get(i as i32).unwrap());
+// 		}
+// 	}
+
+// 	#[test]
+// 	fn test_reverse_linked_list_2() {
+// 		let mut list = LinkedList::<i32>::new();
+// 		let original_vec = vec![34,56,78,25,90,10,19,34,21,45];
+// 		let reverse_vec = vec![45,21,34,19,10,90,25,78,56,34];
+// 		for i in 0..original_vec.len(){
+// 			list.add(original_vec[i]);
+// 		}
+// 		println!("Linked List is {}", list);
+// 		list.reverse();
+// 		println!("Reversed Linked List is {}", list);
+// 		for i in 0..original_vec.len(){
+// 			assert_eq!(reverse_vec[i],*list.get(i as i32).unwrap());
+// 		}
+// 	}
+// }
 
 mod prime_factor;
 
@@ -31,5 +200,39 @@ fn add<'a>(str1: &'a str, str2: &'a str)->&'a str{
     }else if check_zero(str2){
         return str1;
     }
+    let mut ans = "";
+    let mut v_str1 = Vec::<char>::new();
+    let mut v_str2 = Vec::<char>::new();
+    for a in str1.chars().into_iter(){
+        v_str1.push(a);
+    }
+    for a in str2.chars().into_iter(){
+        v_str2.push(a);
+    }
+    let mut i = v_str1.len() - 1;
+    let mut j = v_str1.len() - 1;
+    let mut flag = 0;
+    while i >= 0 || j >= 0 || flag != 0 {
+        let mut numa = 0;
+        let mut numb = 0;
+        let mut s_a = "";
+        let mut s_b = "";
+        if i >= 0{
+            s_a.to_string().push(v_str1[i]);
+            s_a.to_string().push('0');
+            numa = s_a.parse().expect("not a number");
+            i -= 1;
+        }
+        if j >= 0{
+            s_b.to_string().push(v_str2[i]);
+            s_b.to_string().push('0');
+            numb = s_b.parse().expect("not a number");
+            j -= 1;
+        }
+        flag = numa + numb + flag;
+        ans.to_string().push('0');
+        ans.to_string().push_str((flag % 10).to_string().as_str());
+    }
+
     unimplemented!()
 }
